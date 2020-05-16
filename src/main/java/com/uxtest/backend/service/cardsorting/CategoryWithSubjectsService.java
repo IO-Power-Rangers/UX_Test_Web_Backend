138ce0ba67f8f2a64fa9378679f4752a949bc12c
misikora <mikolajsi@student.agh.edu.pl>
2020-05-16 10:12:47 +0200

    Fix

--- src/main/java/com/uxtest/backend/service/cardsorting/CategoryWithSubjectsService.java
+++ src/main/java/com/uxtest/backend/service/cardsorting/CategoryWithSubjectsService.java
@@ -3,15 +3,12 @@ package com.uxtest.backend.service.cardsorting;
 import com.uxtest.backend.model.cardsorting.Category;
 import com.uxtest.backend.model.cardsorting.CategoryWithSubjects;
 import com.uxtest.backend.model.cardsorting.Subject;
-import com.uxtest.backend.repository.cardsorting.CategoryRepository;
 import com.uxtest.backend.repository.cardsorting.CategoryWithSubjectsRepository;
-import com.uxtest.backend.repository.cardsorting.SubjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
-import java.util.Arrays;
 import java.util.List;
 
 @Service
@@ -37,7 +34,6 @@ public class CategoryWithSubjectsService {
         subjects.forEach(s -> {
             Subject subject = subjectService.getSubjectById(s.getId());
             subject.addCategory(category);
-            subjectService.createSubject(subject);
         });
     }
 
