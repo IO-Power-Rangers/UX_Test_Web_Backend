cb77886a027d779677302a63b7adc835399cea55
misikora <mikolajsi@student.agh.edu.pl>
2020-05-16 17:14:45 +0200

    Fix

--- src/main/java/com/uxtest/backend/service/cardsorting/CategoryWithSubjectsService.java
+++ src/main/java/com/uxtest/backend/service/cardsorting/CategoryWithSubjectsService.java
@@ -9,6 +9,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.util.Arrays;
+import java.util.Iterator;
 import java.util.List;
 
 @Service
